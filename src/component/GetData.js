571e6143be3cbcedecf769ddba29c8591b4aaf2a
Priya Ramachanriya Surendranath <priyami2010@gmail.com>
2021-10-05 15:24:01 -0400

    refactoring box/getdata js

--- src/component/GetData.js
+++ src/component/GetData.js
@@ -3,25 +3,29 @@ import { Card } from 'react-bootstrap';
 import { WiDayThunderstorm } from "react-icons/wi";
 
 
-const GetData = (props) => {
+const GetData = (data) => {
 
-    // console.log(props.data.name, 'get data')
+    var combinedData = {...data.data[0], ...data.data[1]}
+    console.log(data.data[0], 'data from get data comp')
+    console.log(combinedData, 'combined')
 
     return (
-        <> 
-            <Card.Body> 
-                             <div>
-                                 <Card.Title> Weather in {props.data.name} </Card.Title>
-                                 <Card.Text>{props.data.temp_f} F </Card.Text>
-                                 <Card.Text><WiDayThunderstorm /> Cloud:{props.data.cloud}</Card.Text>
-                                 <Card.Text>Humidity:{props.data.humidity}</Card.Text>
-                                 <Card.Text>Precipition:</Card.Text>
-                                 <Card.Text>Moisture:</Card.Text>
-                             </div> 
-   
-            </Card.Body>
-        </>
+        
+        <div>
+               
+                {
+                     <Card.Body >
+                             <Card.Title> Weather in {combinedData.name} </Card.Title>
+                             <Card.Text>{combinedData.temp_f} F </Card.Text>
+                             <Card.Text><WiDayThunderstorm />{combinedData.text} Cloud:{combinedData.cloud}</Card.Text>
+                             <Card.Text>Humidity:{combinedData.humidity}</Card.Text>
+                             <Card.Text>Perciptition:</Card.Text>
+                             <Card.Text>Moisture:</Card.Text>
+                    </Card.Body>
+                }
+        </div>
     )
+
 }
 
 export default GetData;
