cdb1ae6e0141846b23eaa56bc6435f15e4c8aff1
Priya Ramachanriya Surendranath <priyami2010@gmail.com>
2021-10-05 14:29:54 -0400

    Conflict resolved

--- src/component/GetData.js
+++ src/component/GetData.js
@@ -3,9 +3,11 @@ import { Card } from 'react-bootstrap';
 import { WiDayThunderstorm } from "react-icons/wi";
 
 
-const GetData = (props) => {
-
-    // console.log(props.data.name, 'get data')
+const GetData = (data) => {
+//data combined to have single json object 
+    var combinedData = {...data.data[0], ...data.data[1]}
+    console.log(data.data[0], 'data from get data comp')
+    console.log(combinedData, 'combined')
 
     return (
         <> 
