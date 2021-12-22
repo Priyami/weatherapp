1fd1fb408c70837ee9391a9a56da61580de04eff
PriyaRSurendranath <priyami2010@gmail.com>
2021-12-21 20:46:45 -0500

    weekweather refactored and styling added

--- src/component/Weekweather.js
+++ src/component/Weekweather.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import { Card, Form, Col, ListGroup } from 'react-bootstrap';
-import moment from 'moment'
+import { Card, ListGroup } from 'react-bootstrap';
+import moment from 'moment';
+import "./Weekweather.css";
 
 const Weekweather = (props) => {
     var combinedData = {...props.data[0], ...props.data[1], ...props.data[2]}
     var forecast = combinedData.forecastday;
     
     function dayDate(date){
-        return new moment(date).format('dddd')
+        return new moment(date).format('ddd')
     }
     return (
         <div>
            <Card  className="bg-dark text-white text-center">
-                <Card.Header>
-                    <Form.Row>
-                        <Form.Group as={Col}>
-                        </Form.Group>
-                    </Form.Row>
-                </Card.Header>
-                <Card.Body>
-                    <ListGroup >
-                        <ListGroup.Item variant="dark">{dayDate(forecast[0].date)}  {forecast[0].day.condition.text}  <img src = {forecast[0].day.condition.icon}/></ListGroup.Item>
-                        <ListGroup.Item variant="dark">{dayDate(forecast[1].date)}  {forecast[1].day.condition.text}  <img src = {forecast[1].day.condition.icon}/></ListGroup.Item>
-                        <ListGroup.Item variant="dark">{dayDate(forecast[2].date)}  {forecast[2].day.condition.text}  <img src = {forecast[2].day.condition.icon}/></ListGroup.Item>
-                     </ListGroup>
+                
+                <Card.Body >
+                    {forecast.map((forecast,id) => (
+                    <ListGroup  className = "list-group-forecast " key = {id} >
+                        <ListGroup.Item className = "bg-dark text-white text-center ">{dayDate(forecast.date)}</ListGroup.Item>
+                        <ListGroup.Item>{forecast.day.condition.text}</ListGroup.Item>
+                        <ListGroup.Item><img src = {forecast.day.condition.icon}/></ListGroup.Item>
+                        </ListGroup>
+                     ))}
                 </Card.Body>
-                <Card.Footer className="text-muted"></Card.Footer>
             </Card>
         </div>
     )
