5cf502f281438cceb42820d3b82079fdd52bce41
PriyaRSurendranath <priyami2010@gmail.com>
2022-01-07 11:10:48 -0500

    useContext hook instead listening through ReactContext

--- src/component/GetData.js
+++ src/component/GetData.js
@@ -1,44 +1,47 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
 import "./GetData.css";
 import DegreeContext from './store/degree-context';
 const GetData = (data) => {
     let combinedData = { ...data.data[0], ...data.data[1], ...data.data[2] }
+    let condition = { ...combinedData.condition };
+    const ctx = useContext(DegreeContext);
     return (
-        <DegreeContext.Consumer>
-            {(ctx) => {
-                return (
-                    <Card.Body >
-                        <Card.Title> Current Weather in {combinedData.name} </Card.Title>
-                        <Card.Text>
-                            <Row>
-                                <Col className="temperature">
-                                    {(ctx.degree === 'Farenheit')
-                                            ?
+        <Card.Body >
+            <Card.Title> Current Weather in {combinedData.name} </Card.Title>
+            <Card.Text>
+                <Row className="justify-content-md-center">
+                    <Col xs lg="2" className="temperature">
+                        {(ctx.degree === 'Farenheit')
+                            ?
 
-                                            <div>
-                                                {combinedData.temp_f}
-                                                <span>&#8457;</span>
-                                            </div>
+                            <div>
+                                {combinedData.temp_f}
+                                <span>&#8457;</span>
+                            </div>
 
-                                            :
-                                            <div>
-                                                {combinedData.temp_c}
-                                                <span>&#8451;</span>
-                                            </div>
-                                    }
+                            :
+                            <div>
+                                {combinedData.temp_c}
+                                <span>&#8451;</span>
+                            </div>
+                        }
 
-                                </Col>
-                            </Row>
-                        </Card.Text>
-                        <Card.Text>Cloud:{combinedData.cloud}</Card.Text>
-                        <Card.Text>Humidity:{combinedData.humidity}</Card.Text>
-                        <Card.Text>Perciptition:{combinedData.precip_mm}</Card.Text>
-                        <Card.Text>{(ctx.degree === 'Farenheit') ? <div>Feelslike:{combinedData.feelslike_f}<span>&#8457;</span></div> : <div>Feelslike:{combinedData.feelslike_c} <span>&#8451;</span></div>}</Card.Text>
-                    </Card.Body>
-                )
-            }}
-        </DegreeContext.Consumer>
+                    </Col>
+                    <Col xs lg="2" >
+                        <Card.Text>  <img src={condition.icon} /> </Card.Text>
+                    </Col>
+                    <Row  >
+                        <Card.Text>  {condition.text} </Card.Text>
+                    </Row>
+                </Row>
+            </Card.Text>
+            <Card.Text>Cloud:{combinedData.cloud}</Card.Text>
+            <Card.Text>Humidity:{combinedData.humidity}</Card.Text>
+            <Card.Text>Perciptition:{combinedData.precip_mm}</Card.Text>
+            <Card.Text>{(ctx.degree === 'Farenheit') ? <div>Feelslike:{combinedData.feelslike_f}<span>&#8457;</span></div> : <div>Feelslike:{combinedData.feelslike_c} <span>&#8451;</span></div>}</Card.Text>
+        </Card.Body>
+
     )
 }
 export default GetData;
--- src/component/Weekweather.js
+++ src/component/Weekweather.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Card, Table } from 'react-bootstrap';
 import Hourlyweather from './Hourlyweather';
 import DegreeContext from './store/degree-context';
@@ -8,58 +8,55 @@ import "./Weekweather.css";
 const Weekweather = (props) => {
     var combinedData = { ...props.data[0], ...props.data[1], ...props.data[2] }
     var forecast = combinedData.forecastday;
+    const ctx = useContext(DegreeContext);
     function dayDate(date) {
         return new moment(date).format('ddd')
     }
 
     return (
-        <DegreeContext.Consumer>
-            {(ctx) => {
-                return (
-                    <div>
-                        <Card className="bg-dark text-white text-center">
 
-                            <Card.Body >
-                                <span>Hourly</span>
-                                <Hourlyweather data={props.data}></Hourlyweather>
-                            </Card.Body>
-                        </Card>
-                        <Card className="bg-dark text-white text-center">
+        <div>
+            <Card className="bg-dark text-white text-center">
 
-                            <Card.Body className="card-body-forecast" >
-                                <Table variant="dark" responsive>
-                                    <tbody>
-                                        {forecast.map((forecast, id) => (
-                                            <td key={id} >
-                                                <td>
-                                                    <tr>{dayDate(forecast.date)}</tr>
-                                                    <tr>{forecast.day.condition.text} </tr>
-                                                    <tr>{(ctx.degree === 'Farenheit')
-                                                        ?
+                <Card.Body >
+                    <span>Hourly</span>
+                    <Hourlyweather data={props.data}></Hourlyweather>
+                </Card.Body>
+            </Card>
+            <Card className="bg-dark text-white text-center">
 
-                                                        <div>
-                                                            {forecast.day.avgtemp_f}
-                                                            <span>&#8457;</span>
-                                                        </div>
+                <Card.Body className="card-body-forecast" >
+                    <Table variant="dark" responsive>
+                        <tbody>
+                            {forecast.map((forecast, id) => (
+                                <td key={id} >
+                                    <td>
+                                        <tr>{dayDate(forecast.date)}</tr>
+                                        <tr>{forecast.day.condition.text} </tr>
+                                        <tr>{(ctx.degree === 'Farenheit')
+                                            ?
 
-                                                        :
-                                                        <div>
-                                                            {forecast.day.avgtemp_c}
-                                                            <span>&#8451;</span>
-                                                        </div>
-                                                    }</tr>
-                                                    <tr><img src={forecast.day.condition.icon} /></tr>
-                                                </td>
-                                            </td>
-                                        ))}
-                                    </tbody>
-                                </Table>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                )
-            }}
-        </DegreeContext.Consumer>
+                                            <div>
+                                                {forecast.day.avgtemp_f}
+                                                <span>&#8457;</span>
+                                            </div>
+
+                                            :
+                                            <div>
+                                                {forecast.day.avgtemp_c}
+                                                <span>&#8451;</span>
+                                            </div>
+                                        }</tr>
+                                        <tr><img src={forecast.day.condition.icon} /></tr>
+                                    </td>
+                                </td>
+                            ))}
+                        </tbody>
+                    </Table>
+                </Card.Body>
+            </Card>
+        </div>
+
     )
 }
 export default Weekweather;
